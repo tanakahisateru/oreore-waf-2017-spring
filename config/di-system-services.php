50bca5877cdd2692d4348219bfc70dcdd35ee79e
Hisateru Tanaka <tanakahisateru@gmail.com>
2017-04-22 07:41:54 +0900

    Delete unused packages.

--- config/di-system-services.php
+++ config/di-system-services.php
@@ -1,11 +1,10 @@
 <?php
 use Aura\Di\Container;
-use Aura\Sql\ExtendedPdo;
 
 /** @var Container $di */
 /** @var array $params */
 
-$di->set('db1', $di->lazyNew(ExtendedPdo::class, [
+$di->set('db1', $di->lazyNew(PDO::class, [
     'dsn' => getenv('MY_APP_DB_DSN'),
     'username' => getenv('MY_APP_DB_USERNAME'),
     'password' => getenv('MY_APP_DB_PASSWORD'),
