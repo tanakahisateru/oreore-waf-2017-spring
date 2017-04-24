9c7c458e801d6510e937c78c155ea356f4f6293f
Hisateru Tanaka <tanakahisateru@gmail.com>
2017-04-24 16:14:00 +0900

    Add acceptance tests for localhost:8080

---
+++ tests/acceptance/ApiCest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class ApiCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function posts(AcceptanceTester $I)
+    {
+        $I->sendGET('/api/posts');
+        $I->seeResponseIsJson([
+            'posts' => [],
+        ]);
+    }
+}
