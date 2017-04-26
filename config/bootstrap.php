5ede538569b1b1871a8bbd8ddb98e21012e4ecc5
Hisateru Tanaka <tanakahisateru@gmail.com>
2017-04-26 22:33:13 +0900

    Skip loading .env when environment vars given

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,14 +2,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // dotenv
-\josegonzalez\Dotenv\Loader::load([
-    'filepaths' => [
-        __DIR__ . '/../.env',
-    ],
-    'putenv' => true,
-    'toEnv' => true,
-]);
-
+if (!getenv('MY_APP_NAME')) {
+    \josegonzalez\Dotenv\Loader::load([
+        'filepaths' => [
+            __DIR__ . '/../.env',
+        ],
+        'putenv' => true,
+        'toEnv' => true,
+    ]);
+}
 // ini
 // error handler
 // polyfills
